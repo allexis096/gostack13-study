4a0875090aad9c44baa38019bc649c71b950c428
allexis096 <allexissoares@gmail.com>
2020-08-31 20:57:52 -0300

    :rocket: chore: interface Dashboard page done

--- 02-project-react/src/pages/Dashboard/index.tsx
+++ 02-project-react/src/pages/Dashboard/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="/">
+          <img
+            src="https://github.com/allexis096.png"
+            alt="Allexis Figueiredo"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>My name is barry Allen!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="/">
+          <img
+            src="https://github.com/allexis096.png"
+            alt="Allexis Figueiredo"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>My name is barry Allen!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="/">
+          <img
+            src="https://github.com/allexis096.png"
+            alt="Allexis Figueiredo"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>My name is barry Allen!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
